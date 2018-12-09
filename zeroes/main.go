d59ffb79d62d1cc8f7959073003d2bf07c114a85
Luciano Ramalho <luciano@ramalho.org>
2018-12-09 09:37:23 -0200

    slides as presented on TDC2018 - Porto Alegre

--- zeroes/main.go
+++ zeroes/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 
 	var anInt int
+	var aFloat32 float32
 	var aString string
 	var anArray [3]float32
 	var anArrayPtr *[3]float32
@@ -15,13 +16,13 @@ func main() {
 	var aMap map[string]int
 	var aChannel chan byte
 
-	fmt.Printf("%T (%d): %#v\n", aString, unsafe.Sizeof(aString), aString)
-	fmt.Printf("%T (%d): %#v\n", anInt, unsafe.Sizeof(anInt), anInt)
-	fmt.Printf("%T (%d): %#v\n", anArray, unsafe.Sizeof(anArray), anArray)
-	fmt.Printf("%T (%d): %#v\n", anArrayPtr, unsafe.Sizeof(anArrayPtr), anArrayPtr)
-	fmt.Printf("%T (%d): %#v\n", aSlice, unsafe.Sizeof(aSlice), aSlice)
-	fmt.Printf("%T (%d): %#v\n", aMap, unsafe.Sizeof(aMap), aMap)
-	fmt.Printf("%T (%d): %#v\n", aChannel, unsafe.Sizeof(aChannel), aChannel)
-
+	fmt.Printf("%T\t%d\t%#v\n", anInt, unsafe.Sizeof(anInt), anInt)
+	fmt.Printf("%T\t%d\t%#v\n", aFloat32, unsafe.Sizeof(aFloat32), aFloat32)
+	fmt.Printf("%T\t%d\t%#v\n", anArray, unsafe.Sizeof(anArray), anArray)
+	fmt.Printf("%T\t%d\t%#v\n", aString, unsafe.Sizeof(aString), aString)
+	fmt.Printf("%T\t%d\t%#v\n", anArrayPtr, unsafe.Sizeof(anArrayPtr), anArrayPtr)
+	fmt.Printf("%T\t%d\t%#v\n", aSlice, unsafe.Sizeof(aSlice), aSlice)
+	fmt.Printf("%T\t%d\t%#v\n", aMap, unsafe.Sizeof(aMap), aMap)
+	fmt.Printf("%T\t%d\t%#v\n", aChannel, unsafe.Sizeof(aChannel), aChannel)
 
 }
